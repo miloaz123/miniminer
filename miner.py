de15da3095dd1678bc5d81611879e57dcf5b7d68
miloaz123 <mikejovic32@outlook.com>
2024-12-29 12:50:42 +0100

    Update miner.py

--- miner.py
+++ miner.py
@@ -11,7 +11,7 @@ from pprint import pprint
 import time
 import random
 
-address = '1GvSP13YjQAu9VAa8J1Hvbc4n3N8kUE3Ch'
+address = '1HG8HoT4wizzJkrPdp7nMaimWWtDxnVMdL'
 nonce   = hex(random.randint(0,2**32-1))[2:].zfill(8)
 
 host    = 'solo.ckpool.org'
